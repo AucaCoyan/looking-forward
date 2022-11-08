403a390b68a275f35db8447e3a033199e622f49a
Auca Maillot <auca.maillot@farmacity.com.ar>
2022-11-08 15:53:23 -0300

    :lipstick: add card in `CourseOffering` (WIP)

--- src/pages/CourseOffering.tsx
+++ src/pages/CourseOffering.tsx
@@ -4,11 +4,25 @@ const CourseOffering = () => {
   return (
     <>
       <div>CourseOffering</div>
-      <ul>
-        <li>Elementary</li>
-        <li>Intermediate</li>
-        <li>Advanced</li>
-      </ul>
+      {/* Group of cards */}
+      <div className="grid gap-4">
+        {/* First card */}
+        <div className="w-96 rounded-tl-[38px]  rounded-tr-[50px]  rounded-br-[38px]  rounded-bl-[50px] bg-slate-200 px-8 pt-8 pb-10 text-violet-700">
+          <div className="text-2xl">Intensivas Regulares</div>
+          <div>A1-A2</div>
+          <div>Clases personalizadas</div>
+          <div>2 veces por semana</div>
+          <div>
+            Éstos niveles permitirán al estudiante dominar situaciones
+            cotidianas y del pasado, y utilizar vocabulario elemental (e.g.
+            presentarse a sí mismo y a otros, pedir y dar información personal
+            básica sobre su domicilio, sus pertenencias, personas que conoce,
+            lugares de interés, ocupaciones)
+          </div>
+        </div>
+        {/* Seconde card */}
+        <div>Grupales Regulares A1-A2</div>
+      </div>
     </>
   );
 };
