e42fae1c07ab8275fc4e66d4e167d9321dbde8f3
Auca Maillot <auca.maillot@farmacity.com.ar>
2022-11-08 14:47:53 -0300

    :sparkles: add section "Nuestra excelencia"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,16 @@ export default function Home() {
         Welcome to our site!
       </div>
       <div className="text-3xl pt-6 text-center">Let&#39;s learn together</div>
+      <section className="pt-10 px-10">
+        <div className="text-3xl">Nuestra excelencia</div>
+        <div className="text-lg py-6">
+          Looking Forward toma como parámetro de máxima exigencia y excelencia
+          en evaluación del idioma internacionalmente al British Council y al
+          CEFR (Common European Framework of Reference for Languages),
+          alineándose año a año con los más altos estándares de calidad en el
+          material y la pedagogía aplicados a nuestras clases.
+        </div>
+      </section>
     </div>
   );
 }
