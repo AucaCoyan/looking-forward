da917a1936e96994f79b2dea930974e864ced194
Auca Maillot <auca.maillot@farmacity.com.ar>
2022-11-08 11:11:29 -0300

    :sparkles: add `CourseOffering`

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         {/* Words navbar */}
         <div className="dark:bg-slate-800 dark:text-cyan-400 px-6 flex items-center m-auto justify-between xl:max-w-4xl sm:max-w-sm">
           <Link href="/">Looking Forward</Link>
-          <div>Course Offering</div>
+          <Link href="/CourseOffering">Course Offering</Link>
           <div>Reviews</div>
           <Link href="/CancellationPolicy">Cancellation Policy</Link>
         </div>
---
+++ src/pages/CourseOffering.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CourseOffering = () => {
+  return (
+    <>
+      <div>CourseOffering</div>
+      <ul>
+        <li>Elementary</li>
+        <li>Intermediate</li>
+        <li>Advanced</li>
+      </ul>
+    </>
+  );
+};
+
+export default CourseOffering;
